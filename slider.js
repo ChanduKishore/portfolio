92b92856b0f27cdc34f1a754d02fb9627be30aec
ChanduKishore <kishorechandu223@gmail.com>
2021-09-27 19:55:40 +0530

    ui update

--- slider.js
+++ slider.js
@@ -1,4 +1,7 @@
-let navBarStatus = (window.innerWidth > 899)? true : false;
+
+const projectsNavBar =document.querySelector('.projects-nav');
+let sliderX =0;
+let refslider=0;
 
 setNavBar();
 
@@ -6,21 +9,13 @@ window.addEventListener('resize',()=>{
 	setNavBar();
 		})
 
-
-
-
 function setNavBar(){
-	if(navBarStatus){
-		 displayProjectNavBar();
-			navBarStatus=false;
-				}
-			else{
-				removeNavBar();
-			}
+	if(window.innerWidth >899){
+		displayProjectNavBar()
+	}else{removeNavBar()}
 }
-
 function displayProjectNavBar(){
-	const projectsNavBar =document.querySelector('.projects-nav');
+			removeNavBar();
 			const projects= document.querySelector('.projects-container');
 			const nProjects = Math.floor(projects.scrollWidth/900);
 		
@@ -31,24 +26,66 @@ function displayProjectNavBar(){
 		
 			}
 
-			let sliderX =0;
+			
 			projects.scrollTo(sliderX,0);
 			const projectNavBtns = document.querySelectorAll('.projects-nav button');
 
-			changeBtnState();
+			changeBtnState(projectNavBtns);
 
 			projectNavBtns.forEach((button, index)=>{
 		
 							button.addEventListener('click',(e)=>{
-								let direction =(sliderX < 900*index)? 'forward': 'backward';
-								scroll(sliderX,direction);
 								sliderX =900*index;
-								changeBtnState();
+								let direction;
+								switch(true){
+									case (sliderX > refslider):
+									direction ='forward';
+									break;
+									case (sliderX < refslider):
+									direction ='backward';
+									break;
+									case (sliderX === refslider):
+									direction ='none';
+									break;
+
+								}
+								 
+								 if(direction!=='none'){
+								 scroll(refslider,direction);}
+								 refslider =sliderX;
+								
+								changeBtnState(projectNavBtns);
+								disableBtns(projectNavBtns);
 								
 								
 							})
 						})
-		
+	
+
+			function scroll(offset,direction){
+				switch(direction){
+					case 'forward':
+					offset+=25;
+					break;
+					case 'backward':
+					offset-=25;
+					break;
+				}
+							
+					changeScroll(offset);
+					const scrolling  = setTimeout(scroll,10,offset,direction);
+					console.log(sliderX,direction,offset,refslider);
+					if(projects.scrollLeft === sliderX){
+							clearTimeout(scrolling);
+							enableBtns(projectNavBtns);
+							}
+			
+			}
+		 function changeScroll(offset){
+				projects.scrollTo(offset,0);
+			}
+
+
 
 		}
 
@@ -60,6 +97,29 @@ function displayProjectNavBar(){
 		}
 		}
 
-
 	
-			
+	
+		function changeBtnState(buttonList){
+			buttonList.forEach((button, index)=>{
+
+				if(index*900 === sliderX){
+							button.setAttribute('class','btn-active');}
+							else{button.setAttribute('class','btn-inactive');}
+			})
+		}
+
+		function disableBtns(btns){
+
+			btns.forEach(btn=>{
+				btn.disabled = true;
+			})
+
+		}
+
+		function enableBtns(btns){
+
+			btns.forEach(btn=>{
+				btn.disabled = false;
+			})
+
+		}
